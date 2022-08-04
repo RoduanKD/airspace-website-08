b08d5b37242439832005a3ef9fb07f83c45ee6ae
Roduan Kareem Aldeen <roduan98@gmail.com>
2022-08-04 19:07:49 +0300

    add initial services page

---
+++ src/views/ServiceView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="service-view">
+    <page-title
+      title="Services"
+      description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, ex!"
+    />
+  </div>
+</template>
+
+<script>
+import PageTitle from '@/components/PageTitle.vue'
+
+export default {
+  components: {
+    PageTitle
+  }
+}
+</script>
